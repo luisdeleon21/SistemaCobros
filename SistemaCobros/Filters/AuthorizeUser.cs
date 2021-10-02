d2f663a7ede62c15cdf5992e0633ffc0b31dd74f
Jonathan <Jonathan@DESKTOP-LPP468N>
2021-10-01 23:22:04 -0600

    Filtros

---
+++ SistemaCobros/Filters/AuthorizeUser.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using SistemaCobros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaCobros.Filters
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class AuthorizeUser : AuthorizeAttribute
+    {
+        private Usuario oUsuario;
+        private sistema_cobrosEntities2 db = new sistema_cobrosEntities2();
+        private int idOperacion;
+
+        public AuthorizeUser(int idOperacion = 0)
+        {
+            this.idOperacion = idOperacion;
+        }
+
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            String nombreOperacion = "";
+            String nombreModulo = "";
+            try
+            {
+                oUsuario = (Usuario)HttpContext.Current.Session["User"];
+                var lstMisOperaciones = from m in db.rol_operacion
+                                        where m.id_rol == oUsuario.id_rol
+                                            && m.operacion_id == idOperacion
+                                        select m;
+
+
+                if (lstMisOperaciones.ToList().Count() == 0)
+                {
+                    var oOperacion = db.Operacion.Find(idOperacion);
+                    int? idModulo = oOperacion.modulo_id;
+
+                    nombreModulo = getNombreDelModulo(idModulo);
+                    filterContext.Result = new RedirectResult("~/Error/UnauthorizedOperation?operacion=" + nombreOperacion + "&modulo=" + nombreModulo + "&msjeErrorExcepcion=");
+                }
+            }
+            catch (Exception ex)
+            {
+                filterContext.Result = new RedirectResult("~/Error/UnauthorizedOperation?operacion=" + nombreOperacion + "&modulo=" + nombreModulo + "&msjeErrorExcepcion=" + ex.Message);
+            }
+        }
+
+
+
+        public string getNombreDelModulo(int? idModulo)
+        {
+            var modulo = from m in db.Modulo
+                         where m.modulo_id == idModulo
+                         select m.nombre;
+            String nombreModulo;
+            try
+            {
+                nombreModulo = modulo.First();
+            }
+            catch (Exception)
+            {
+                nombreModulo = "";
+            }
+            return nombreModulo;
+        }
+
+    }
+}
---
+++ SistemaCobros/Filters/VerificaUsuario.cs
@@ -0,0 +1,41 @@
+﻿using SistemaCobros.Controllers;
+using SistemaCobros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaCobros.Filters
+{
+    public class VerificaUsuario : ActionFilterAttribute
+    {
+        private Usuario oUsuario;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+
+                oUsuario = (Usuario)HttpContext.Current.Session["User"];
+                if (oUsuario == null)
+                {
+
+                    if (filterContext.Controller is AccesoController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Acceso/Login");
+                    }
+
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Acceso/Login");
+            }
+
+        }
+    }
+}
